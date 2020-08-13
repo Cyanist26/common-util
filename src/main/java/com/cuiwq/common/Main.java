31fe642209fd94ac0a2048f5c55c2ecb90a933d8
cuiweiqiang <cuiwq@xiaomodo.com>
2020-08-13 19:02:23 +0800

    hw

--- src/main/java/com/cuiwq/common/Main.java
+++ src/main/java/com/cuiwq/common/Main.java
@@ -3,9 +3,187 @@ package com.cuiwq.common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    
+    /* HJ23 删除字符串中出现次数最少的字符 */
+    public static void main(String[] args) throws IOException {
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        while((str = b.readLine()) != null) {
+            Arrays.stream(str.split("")).
+        }
+
+    }
+    
+    /* HJ22 汽水瓶 */
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+    //     String str = null;
+    //     while((str = b.readLine()) != null && !"0".equals(str)) {
+    //         int count = Integer.parseInt(str);
+    //         int result = 0;
+    //         while(count >= 3) {
+    //             int cur = count / 3;
+    //             result += cur;
+    //             count = cur + count % 3;
+    //         }
+    //         if(count == 2) {
+    //             result += 1;
+    //         }
+    //         System.out.println(result);
+    //     }
+    // }
+    
+    /* HJ21 简单密码 */
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+    //     String str = null;
+    //     while((str = b.readLine()) != null) {
+    //         StringBuilder sb = new StringBuilder();
+    //         for(char c : str.toCharArray()) {
+    //             if(c == 'Z') {
+    //                 sb.append('a');
+    //             } else if(c >= 'A' && c <= 'Y') {
+    //                 sb.append((char) Character.toLowerCase(c + 1));
+    //             } else if(c >= 'a' && c <= 'z') {
+    //                 if(c <= 'l') {
+    //                     if(c <= 'f') {
+    //                         if(c <= 'c') {
+    //                             sb.append("2");
+    //                         } else {
+    //                             sb.append("3");
+    //                         }
+    //                     } else {
+    //                         if(c <= 'i') {
+    //                             sb.append("4");
+    //                         } else {
+    //                             sb.append("5");
+    //                         }
+    //                     }
+    //                 } else {
+    //                     if(c <= 's') {
+    //                         if(c <= 'o') {
+    //                             sb.append("6");
+    //                         } else {
+    //                             sb.append("7");
+    //                         }
+    //                     } else {
+    //                         if(c <= 'v') {
+    //                             sb.append("8");
+    //                         } else {
+    //                             sb.append("9");
+    //                         }
+    //                     }
+    //                 }
+    //             } else {
+    //                 sb.append(c);
+    //             }
+    //         }
+    //         System.out.println(sb.toString());
+    //     }
+    // }
+    
+    /* HJ20 验证密码 */
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+    //     String str = null;
+    //     StringBuilder sb = new StringBuilder();
+    //     while((str = b.readLine()) != null) {
+    //         if(str.length() <= 8) {
+    //             sb.append("NG\n");
+    //             continue;
+    //         }
+    //         int up = 0, low = 0, num = 0, other = 0;
+    //         for(int i = 0; i < str.length(); i++) {
+    //             char c = str.charAt(i);
+    //             if(up == 0 && c >= 'A' && c <= 'Z') {
+    //                 up = 1;
+    //             } else if(low == 0 && c >= 'a' && c <= 'z') {
+    //                 low = 1;
+    //             } else if(num == 0 && c >= '0' && c <= '9') {
+    //                 num = 1;
+    //             } else {
+    //                 other = 1;
+    //             }
+    //             if(i < str.length() - 3 && str.indexOf(str.substring(i, i + 3), i + 1) > 0) {
+    //                 other = -10;
+    //                 break;
+    //             }
+    //         }
+    //         if(up + low + num + other < 3) {
+    //             sb.append("NG\n");
+    //         } else {
+    //             sb.append("OK\n");
+    //         }
+    //     }
+    //     System.out.println(sb.toString());
+    // }
+    
+    /* HJ19 简单错误记录 */
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+    //     String str = null;
+    //     Map<String, Integer> result = new LinkedHashMap<>();
+    //     while((str = b.readLine()) != null && !"".equals(str)) {
+    //         String[] record = str.split("\\s+");
+    //         String path = record[0];
+    //         int nameLength = path.length() - 1 - path.lastIndexOf("\\");
+    //         String fileName;
+    //         if(nameLength > 16) {
+    //             fileName = path.substring(path.length() - 16);
+    //         } else {
+    //             fileName = path.substring(path.lastIndexOf("\\") + 1);
+    //         }
+    //         result.merge(fileName + " " + record[1], 1, Integer::sum);
+    //     }
+    //     int i = result.size() - 8;
+    //     int count = 0;
+    //     for(Map.Entry<String, Integer> entry : result.entrySet()) {
+    //         if(count >= i) {
+    //             System.out.println(entry.getKey() + " " + entry.getValue());
+    //         }
+    //         count++;
+    //     }
+    // }
+    
+    /* HJ17 坐标移动 */
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+    //     Pattern p = Pattern.compile("^(?<d>([ADSW]))(?<s>(\\d{1,2}))$");
+    //     String str = null;
+    //     while((str = b.readLine()) != null) {
+    //         int x = 0;
+    //         int y = 0;
+    //         String[] arr = str.split(";");
+    //         for(String move : arr) {
+    //             Matcher m = p.matcher(move);
+    //             if(m.matches()) {
+    //                 String d = m.group("d");
+    //                 int s = Integer.parseInt(m.group("s"));
+    //                 switch(d) {
+    //                     case "A":
+    //                         x -= s;
+    //                         break;
+    //                     case "D":
+    //                         x += s;
+    //                         break;
+    //                     case "S":
+    //                         y -= s;
+    //                         break;
+    //                     case "W":
+    //                         y += s;
+    //                         break;
+    //                 }
+    //             }
+    //         }
+    //         System.out.println(x + "," + y);
+    //     }
+    // }
     
     /* HJ15 二进制1的个数 */
     // public static void main(String[] args) throws IOException {
